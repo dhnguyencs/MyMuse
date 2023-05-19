eba677c3f77d1db71fb8ab16f3e4ddcb6655058c
renelync <99835887+renelync@users.noreply.github.com>
2023-05-18 19:34:42 -0700

    Update HomeController.cs

--- FinalProject_340/Controllers/HomeController.cs
+++ FinalProject_340/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using FinalProject_340.Middleware;
 using FinalProject_340.Models;
-using FinalProject_340.Tests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Tokens;
