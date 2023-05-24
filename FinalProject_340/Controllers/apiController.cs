153f8efd4baed3b3ba3139d0667bec6a43cc891a
renelync <99835887+renelync@users.noreply.github.com>
2023-05-24 01:34:14 -0700

    Adding column for User_Background uploads

--- FinalProject_340/Controllers/apiController.cs
+++ FinalProject_340/Controllers/apiController.cs
@@ -4,11 +4,47 @@ using FinalProject_340.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Drawing;
 using System.Drawing.Imaging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+
 
 namespace FinalProject_340.Controllers
 {
     public class apiController : Controller
     {
+        private readonly IWebHostEnvironment _environment;
+
+        public apiController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                string uploadsFolder = Path.Combine(_environment.WebRootPath, "uploads");
+                string uniqueFileName = Path.GetRandomFileName() + "_" + file.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+
+                // Save the file path in the user's session or database for future use
+                // Example: HttpContext.Session.SetString("BackgroundImagePath", filePath);
+
+                // Set the background image dynamically
+                // Example: ViewBag.BackgroundImagePath = filePath;
+            }
+
+            return RedirectToAction("Index");
+        }
+
         [HttpPost]
         public IActionResult uploadTrack([FromForm] _n_song song)
         {
--- userDB_340/DataTables/users.sql
+++ userDB_340/DataTables/users.sql
@@ -4,5 +4,6 @@
 	UUID			varchar(512)		unique							not null,
 	FIRST_NAME		varchar(30)											not null,
 	LAST_NAME		varchar(30)											not null,
-	EMAIL			varchar(128)		unique							not null,
+	EMAIL			varchar(128)		unique							not null, 
+    [USER_BACKGROUND] IMAGE NULL,
 )
